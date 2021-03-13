fb3bb4cd8eeff4a585ba1c79bcd8ff9bca511867
Daniel <triotk@gmail.com>
2021-03-14 07:26:47 +0900

    tsst

--- apps/test-next/pages/foo/bar.tsx
+++ apps/test-next/pages/foo/bar.tsx
@@ -5,7 +5,7 @@ const Bar = () => {
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <Typography>THIS IS BAR PAGE!</Typography>
-      <Typography>FOO BAZ HAS CHANGED</Typography>
+      <Typography>FOO BAZ HAS CHANGED 2</Typography>
     </div>
   );
 };
