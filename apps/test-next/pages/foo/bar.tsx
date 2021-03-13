5f7c950befc12430716fc78dc633d2dd74bcd4d0
Daniel <triotk@gmail.com>
2021-03-14 07:16:47 +0900

    diff test

--- apps/test-next/pages/foo/bar.tsx
+++ apps/test-next/pages/foo/bar.tsx
@@ -5,6 +5,7 @@ const Bar = () => {
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <Typography>THIS IS BAR PAGE!</Typography>
+      <Typography>FOO BAZ HAS CHANGED</Typography>
     </div>
   );
 };
---
+++ ci/test-next.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+nx run test-next:build --prod
+cd dist/apps/test-next
+npx add-dependencies graphql @emotion/react @emotion/styled
+NEXT_CONTAINER_NAME=test-next
+GCR_PATH=gcr.io/$GC_PROJECT_ID/$NEXT_CONTAINER_NAME:latest
+REGION=asia-northeast3
+SERVICE=$NEXT_CONTAINER_NAME
+
+echo ${GC_PROJECT_ID}
+echo ${NEXT_CONTAINER_NAME}
+echo ${GCR_PATH}
+
+
+cp ../../../apps/test-next/Dockerfile .
+
+docker build . -t $NEXT_CONTAINER_NAME
+docker tag $NEXT_CONTAINER_NAME $GCR_PATH
+
+docker push $GCR_PATH
+
+echo "DOCKER IMAGE PUSHED TO REPO"
+
+gcloud beta run deploy $SERVICE --image $GCR_PATH --project $GC_PROJECT_ID --platform managed --region $REGION --allow-unauthenticated
