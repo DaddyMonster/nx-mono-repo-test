6bb8fe361dcd13a6a1a674e6572a33565e6e02db
Daniel <triotk@gmail.com>
2021-03-15 16:38:22 +0900

    ci-test-34

---
+++ apps/test-nest/test-nest.Dockerfile
@@ -0,0 +1,26 @@
+#--- CREATING BASE
+
+FROM node:12-alpine AS base
+ARG NODE_ENV
+ARG BUILD_FLAG
+WORKDIR /app/cache
+COPY package.json .
+
+
+#--- DEPENDENCIES
+
+FROM base AS dependencies
+RUN npm set progress=false && npm config set depth 0
+RUN npm install --only=production
+
+#--- SOME TEST HERE
+###################
+
+#--- DEPLOY
+FROM base as deploy 
+WORKDIR /app
+COPY --from=dependencies /app/cache/node_modules ./node_modules
+COPY . .
+EXPOSE 3333
+ENV NODE_ENV=$NODE_ENV
+CMD ["node", ".app/main.js"]
--- ci/test-nest.sh
+++ ci/test-nest.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-nx run test-next:build --prod
+
+nx run test-nest:build --prod
+cd dist/apps/test-nest
+npx add-dependencies graphql reflect-metadata
+
 
 #ENVS
 NEST_CONTAINER_NAME=test-nest
@@ -11,12 +15,13 @@ echo ${GC_PROJECT_ID}
 echo ${NEST_CONTAINER_NAME}
 echo ${GCR_PATH}
 
-cd apps/test-nest
-docker build . -t $NEST_CONTAINER_NAME
+
+cp ../../../apps/test-nest/test-nest.Dockerfile .
+docker build -t . $NEST_CONTAINER_NAME -f ./test-nest.Dockerfile
 docker tag $NEST_CONTAINER_NAME $GCR_PATH
 
 docker push $GCR_PATH
 echo "NEST SERVER DOCKER IMAGE PUSHED TO REPO"
 gcloud components install beta --quiet
 
-gcloud beta run deploy $SERVICE --image $GCR_PATH --project $GC_PROJECT_ID --platform managed --region $REGION --allow-unauthenticated
+gcloud beta run deploy $SERVICE --image $GCR_PATH --project $GC_PROJECT_ID --platform managed --region $REGION --allow-unauthenticated --update-env-vars=$TEST_NEST_SECRETS
